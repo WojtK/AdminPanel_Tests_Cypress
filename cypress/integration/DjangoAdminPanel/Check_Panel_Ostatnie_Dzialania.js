d1fd522551953b73370d00f54c3fc47d38500653
WojtK <wojtek.kucharekos@gmail.com>
2021-02-28 01:07:51 +0100

    renaming of tests

--- cypress/integration/DjangoAdminPanel/Check_Panel_Ostatnie_Dzialania.js
+++ cypress/integration/DjangoAdminPanel/Check_Panel_Ostatnie_Dzialania.js
@@ -5,23 +5,23 @@ function Log_In(login, password) {
     cy.get('[value="Zaloguj się"]').click()
 }
 
-describe('Add and remove user "cypres"', () => {
+describe('Check panel "Ostatnie dzialania"', () => {
     beforeEach(() => {
         Log_In("admin", "admin")
     })
-    it('Add user "cypres"', () => {
+    it('Add user "panel"', () => {
         cy.get('[href="/admin/auth/"]').click()
         cy.get('[href="/admin/auth/user/add/"]').click()
-        cy.get('[id="id_username"]').type("cypres")
+        cy.get('[id="id_username"]').type("panel")
         cy.get('[id="id_password1"]').type("TestTest10!")
         cy.get('[id="id_password2"]').type("TestTest10!")
         cy.get('[value="Zapisz"]').click()
         cy.get('[class="success"]').contains('został dodany pomyślnie')
     })
-    it('Delete user "cypres"', () => {
+    it('Delete user "panel"', () => {
         cy.get('[href="/admin/auth/"]').click()
         cy.get('[href="/admin/auth/user/"').first().click()
-        cy.get('[id="searchbar"]').type("cypres")
+        cy.get('[id="searchbar"]').type("panel")
         cy.get('[value="Szukaj"]').click()
         cy.get('[class="action-checkbox"]').click()
         cy.get('[name="action"]').select('delete_selected')
@@ -30,5 +30,10 @@ describe('Add and remove user "cypres"', () => {
         cy.get('[title="Wykonaj wybraną akcję"]').click()
         cy.get('[value="Tak, na pewno"]').click()
         cy.get('[class="success"]').contains('Pomyślnie usunięto')
+    })
+    it('Check Panel "Ostatnie dzialania"', () => {
+        cy.get('[href="/admin/"').click()
+        cy.get('#recent-actions-module > ul > li:nth-child(1)').contains("panel")
+        cy.get('#recent-actions-module > ul > li:nth-child(2)').contains("panel")
     })
 })
